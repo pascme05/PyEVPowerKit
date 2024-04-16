Fixing bug average

src/plot/plotVeh.py
@@ -37,6 +37,7 @@ Outputs:    1)
 # External
 # ==============================================================================
 import matplotlib.pyplot as plt
+import numpy as np
 
 #######################################################################################################################
 # Additional Functions
@@ -50,6 +51,8 @@ def plotVeh(data, dataTime, setup):
     print("INFO: Plotting vehicle data")
 
     eff = setup['Par']['VEH']['eta'] * 100
+    etaT = dataTime['GBX']['T']['eta'] * dataTime['EMA']['T']['eta'] * dataTime['INV']['T']['eta'] * dataTime['HVS']['eta']
+    etaTmean = np.nanmean(etaT) * 100
     time = data['t']
 
     fig, axs = plt.subplots(4, 1, sharex=True)
@@ -65,7 +68,8 @@ def plotVeh(data, dataTime, setup):
     axs[1].grid(True)
 
     axs[2].plot(time, dataTime['VEH']['E']['t'] / 3.6e6, label='Vehicle Energy (100% Rec Efficiency)')
-    axs[2].plot(time, dataTime['VEH']['E']['rec_on'] / 3.6e6, label='Vehicle Energy (' + str(eff) + '% Rec Efficiency)')
+    axs[2].plot(time, dataTime['VEH']['E']['rec_on'] / 3.6e6, label='Vehicle Energy (' + str(int(eff)) + '% Rec Efficiency)')
+    axs[2].plot(time, dataTime['VEH']['E']['t'] / etaT / 3.6e6, label='Vehicle Energy (' + str(int(etaTmean)) + '% Rec Efficiency)')
     axs[2].plot(time, dataTime['VEH']['E']['rec_off'] / 3.6e6, label='Vehicle Energy (0% Rec Efficiency)')
     axs[2].set_ylabel('E (kWh)')
     axs[2].set_title('Vehicle Energy')
@@ -73,7 +77,8 @@ def plotVeh(data, dataTime, setup):
     axs[2].grid(True)
 
     axs[3].plot(time, dataTime['VEH']['eta']['t'], label='Vehicle Consumption (100% Rec Efficiency)')
-    axs[3].plot(time, dataTime['VEH']['eta']['rec_on'], label='Vehicle Consumption (' + str(eff) + '% Rec Efficiency)')
+    axs[3].plot(time, dataTime['VEH']['eta']['rec_on'], label='Vehicle Consumption (' + str(int(eff)) + '% Rec Efficiency)')
+    axs[3].plot(time, dataTime['VEH']['eta']['t'] / etaT, label='Vehicle Consumption (' + str(int(etaTmean)) + '% Rec Efficiency)')
     axs[3].plot(time, dataTime['VEH']['eta']['rec_off'], label='Vehicle Consumption (0% Rec Efficiency)')
     axs[3].set_ylabel('Eta (kWh/100 km)')
     axs[3].set_xlabel('time (sec)')